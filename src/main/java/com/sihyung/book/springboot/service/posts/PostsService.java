de8cabcdec0e71afdb2d3f9e6c3fefbab18c9b6c
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-07-12 12:39:58 +0900

    feat : 게시글 저장로직 개발

---
+++ src/main/java/com/sihyung/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.sihyung.book.springboot.service.posts;
+
+import com.sihyung.book.springboot.domain.posts.PostsRepository;
+import com.sihyung.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
